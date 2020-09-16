9c24661f23f8d8591673987b65b3aa1b8eb1697b
Dudeicle <brian-griffiths@lambdastudents.com>
2020-09-16 16:59:35 -0400

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,74 @@
 module.exports = {
-  success,
-  fail,
-  repair,
-  get,
+	success,
+	fail,
+	repair,
+	get,
 };
 
 function success(item) {
-  return { ...item };
-}
-
-function fail(item) {
-  return { ...item };
-}
+	if (item.enhancement < 20) {
+		item.enhancement = item.enhancement + 1;
+	} else {
+		console.log("Item cannot be enhanced any further!");
+	}
+	return { ...item };
+} // WORKING
 
 function repair(item) {
-  return { ...item };
+	if (item) {
+		console.log("Item Repair successful!");
+		item.durability = 100;
+	} else {
+		console.log("Item Repair failed! Already fully repaired!");
+		item.durability = item.durability;
+	}
+	return { ...item };
+} // WORKING
+
+function fail(item) {
+	// enhancement is below 15
+	if (item.enhancement < 15) {
+		console.log("FAILURE - enhancement below 15!");
+		if (item.durability > 5) {
+			item.durability = item.durability - 5;
+		} else {
+			item.durability = item.durability - item.durability;
+		}
+	}
+	// enhancement is 15
+	else if (item.enhancement === 15) {
+		console.log("FAILURE - enhancement is 15 or 16");
+		if (item.durability > 10) {
+			item.durability = item.durability - 10;
+		} else {
+			item.durability = item.durability - item.durability;
+		}
+	}
+	// enhancement is 16
+	else if (item.enhancement === 16) {
+		console.log("FAILURE - enhancement is 15 or 16");
+		if (item.durability > 10) {
+			item.durability = item.durability - 10;
+		} else {
+			item.durability = item.durability - item.durability;
+		}
+	}
+	// enhancement is over 16
+	else if (item.enhancement > 16) {
+		console.log("FAILURE - enhancement is over 16!");
+		item.enhancement = item.enhancement - 1;
+		if (item.durability > 10) {
+			item.durability = item.durability - 10;
+			console.log("durability was above 10");
+		} else {
+			item.durability = item.durability - item.durability;
+			console.log("durability was below 10");
+		}
+	}
+
+	return { ...item };
 }
 
 function get(item) {
-  return { ...item };
+	return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,71 @@
-const enhancer = require('./enhancer.js');
+const { success, repair, fail } = require("./enhancer.js");
+const enhancer = require("./enhancer.js");
 // test away!
+
+let item = {
+	name: "EpicLoot",
+	durability: 50,
+	enhancement: 13,
+};
+
+describe("enhancer", () => {
+	describe("success", () => {
+		// TESTING THE SUCCESS FUNCTION == WORKING
+		it("should increase enhancement by 1 if not currently at 20", () => {
+			success(item);
+			expect(item.enhancement).toBe(14);
+		});
+	});
+});
+
+// AFTER THIS TEST NEW OBJECT IS
+// let item = {
+// 	name: "EpicLoot",
+// 	durability: 50,
+// 	enhancement: 14,
+// };
+
+describe("enhancer", () => {
+	describe("repair", () => {
+		// TESTING THE REPAIR FUNCTION == WORKING
+		it("should have it's item durability returned to 100", () => {
+			repair(item);
+			expect(item.durability).toBe(100);
+		});
+	});
+});
+
+// AFTER THIS TEST NEW OBJECT IS
+// let item = {
+// 	name: "EpicLoot",
+// 	durability: 100,
+// 	enhancement: 14,
+// };
+
+describe("enhancer", () => {
+	describe("failure", () => {
+		// TESTING THE FAILURE FUNCTION == WORKING
+		it("should fail and remove 1 from enhance and 10 from durability", async () => {
+			success(item); // 15
+			success(item); // 16
+			success(item); // 17
+			success(item); // 18
+
+			// Sanity checks = passed
+			expect(item.durability).toBe(100);
+			expect(item.enhancement).toBe(18);
+
+			fail(item);
+
+			expect(item.durability).toBe(90);
+			expect(item.enhancement).toBe(17);
+		});
+	});
+});
+
+// AFTER THIS TEST NEW OBJECT IS
+// let item = {
+// 	name: "EpicLoot",
+// 	durability: 90,
+// 	enhancement: 17,
+// };
